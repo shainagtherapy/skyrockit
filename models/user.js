29f51cad0ee05ac513a04f891782981ab3ef337e
Shaina G <shainagtherapy@gmail.com>
2025-07-23 10:10:41 -0700

    application schema setup

--- models/user.js
+++ models/user.js
@@ -1,5 +1,27 @@
 const mongoose = require("mongoose");
 
+const applicationSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+    },
+    postingLink: {
+        type: String,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+    },
+})
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -8,7 +30,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    applications: [applicationSchema], // embeds applicationSchema here
 });
 
 const User = mongoose.model('User', userSchema);
