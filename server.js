852aec3ad21b9d16ec6da438fd97cdef369d3a44
Shaina G <shainagtherapy@gmail.com>
2025-07-23 10:45:36 -0700

    homepage build

--- server.js
+++ server.js
@@ -42,15 +42,7 @@ app.get("/", async (req, res) => {
     })
 })
 
-
-app.get("/vip-lounge", (req, res) => {
-    if (req.session.user) {
-        res.send(`Welcome to the party ${req.session.user.username}`)
-    } else {
-        res.send("Sorry, no guests allowed.");
-    }
-})
-
+app.use('/auth', authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
