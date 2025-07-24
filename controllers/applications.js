8198b6b28c33300dbd87042148f5d8592a7f703b
Shaina G <shainagtherapy@gmail.com>
2025-07-24 09:18:19 -0700

    fixed errors

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-// const User = require('../models/users.js')
+const User = require('../models/user.js')
 
 router.get('/', (req, res) => {
     try {
         res.render('applications/index.ejs')
-    } catch (error) {}
+    } catch (error) {
         console.log(error)
         res.redirect('/')
+    }
 });
 
 module.exports = router;
