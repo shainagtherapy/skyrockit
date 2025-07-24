50ad945ba1f2aadefe5a88d7865593ac8ada5f1a
Shaina G <shainagtherapy@gmail.com>
2025-07-24 10:30:00 -0700

    Completed new application function

--- controllers/applications.js
+++ controllers/applications.js
@@ -7,9 +7,30 @@ router.get('/', (req, res) => {
     try {
         res.render('applications/index.ejs')
     } catch (error) {
-        console.log(error)
-        res.redirect('/')
+        console.log(error);
+        res.redirect('/');
     }
 });
+
+// GET for /new
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
+});
+
+// POST for /applications'
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+
+        currentUser.applications.push(req.body);
+
+        await currentUser.save();
+
+        res.redirect(`/users/${currentUser._id}/applications`)
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
 
 module.exports = router;
